1d98098a1d3beaa05b0948c1790d6d69700cae88
Satyam Mishra <sm6003830@gmail.com>
2023-02-08 20:45:55 +0530

    First_Commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import './Home.css'
+
+
+const Home = () => {
+  return (
+    <div className='main'>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='main1'>
+      <div>
+        Vidyalankar Institute Of Technology <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; PRESENTS
+      </div>
+      <div>
+        ALGORYTHM
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default Hero
